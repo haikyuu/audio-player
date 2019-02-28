9c4d887c15003e0f13ffea2aece46d4118c0bad2
Abdellah Alaoui <als.abdellah@gmail.com>
2019-03-01 00:32:47 +0100

    implement tests: get all songs and get song by id

---
+++ src/db/index.test.js
@@ -0,0 +1,29 @@
+import { getAllSongs, getSongById, loadDb } from './'
+import SONGS from './songs.json'
+import { expect } from 'chai'
+
+// can change data at will in tests
+// using "production" data ^^' by default
+loadDb(SONGS)
+
+describe('getting songs metadata', () => {
+    describe('get all songs', () => {
+        it('should return all songs', () => {
+            const songs = getAllSongs()
+            expect(songs)
+                .to.be.an('array')
+                .that.has.length(2)
+        })
+    })
+
+    describe('get song by id', () => {
+        it('gets actual song if exists', () => {
+            const song = getSongById('1')
+            expect(song).to.have.property('id', '1')
+        })
+        it('returns undefined for not found songs', () => {
+            const song = getSongById('4')
+            expect(song).to.be.a('undefined')
+        })
+    })
+})
