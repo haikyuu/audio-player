bdfb091d8f3d1aa16f80fa4a9071769be91becef
Abdellah Alaoui <als.abdellah@gmail.com>
2019-02-28 23:55:16 +0100

    add error handler to get song by id
    - refactor response shape to be able to provide information to the
    client

--- src/routes/songs.js
+++ src/routes/songs.js
@@ -5,11 +5,15 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
     const songs = db.getAllSongs()
-    res.status(200).send(songs)
+    res.status(200).send({ data: songs })
 })
 router.get('/:id', (req, res) => {
     const { id } = req.params
     const song = db.getSongById(id)
-    res.status(200).send(song)
+    if (!song) {
+        return res.status(404).send({ error: 'resource not found' })
+    }
+
+    res.status(200).send({ data: song })
 })
 export default router
